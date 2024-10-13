3b0d679c11caf3174159f1cf479abcedcbe46731
Rahul Badenkal <rahulbadenkal@gmail.com>
2024-10-13 16:04:36 +0530

    Added posthog

--- demo/src/constants.ts
+++ demo/src/constants.ts
@@ -1,1 +1,3 @@
 export const SIGNALLING_SERVER = import.meta.env.VITE_SIGNALLING_SERVER;
+export const POSTHOG_KEY = import.meta.env.VITE_POSTHOG_KEY
+export const POSTHOG_HOST = "https://us.i.posthog.com"
--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import posthog from 'posthog-js'
+import { POSTHOG_HOST, POSTHOG_KEY } from './constants.ts'
+
+posthog.init(POSTHOG_KEY,
+  {
+      api_host: POSTHOG_HOST,
+      person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+  }
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
